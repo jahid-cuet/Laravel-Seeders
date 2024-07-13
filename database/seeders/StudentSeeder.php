2bb551dd1de6e00f416d1def83f5c548ee81232e
Md.Jahid Hasan <jahidhasan5803733@gmail.com>
2024-07-13 15:51:18 +0600

    create StudentSeeder by commanding 'php artisan make:seeder StudentSeeder' and create something in it.

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Student;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Student::create([
+            'name'=>'jahid Hasan',
+            'email'=>'jahid@gmail.com',
+
+        ]);
+    }
+}
